6a3c44c634edf74fecda0e252f3e6153ac78f8ae
dorivaldovalentim <dorivaldovalentim@gmail.com>
2026-01-29 15:06:16 +0100

    feat: adding the basic skeleton structure

---
+++ resources/js/layouts/Dashboard.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+import Menu from '@/components/Menu.vue';
+import Footer from '@/components/Footer.vue';
+
+interface Props {
+	title: string;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+	<Head :title="title"></Head>
+
+	<a href="#content" class="visually-hidden skip-link">Skip to main content</a>
+
+	<div class="page">
+		<Menu />
+
+		<div class="page-wrapper">
+			<slot name="header"></slot>
+
+			<main id="content" class="page-body">
+				<slot name="body"></slot>
+			</main>
+			
+			<Footer />
+		</div>
+
+		<slot name="extra"></slot>
+	</div>
+</template>
