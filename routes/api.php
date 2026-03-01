a81a2139fd7eabf8301b381ae9fe38a77152810a
dorivaldovalentim <dorivaldovalentim@gmail.com>
2026-03-01 17:28:28 +0100

    feat: adding names to api routes

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,10 @@ use App\Http\Controllers\Api\Auth\FirebaseAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::post('/auth/firebase', FirebaseAuthController::class);
+Route::name('api.')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:sanctum');
+    
+    Route::post('/auth/firebase', FirebaseAuthController::class)->name('auth.firebase');
+});
