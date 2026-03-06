f88271d9be6fb2eb312b7cdc93d4e0b7d122df35
dorivaldovalentim <dorivaldovalentim@gmail.com>
2026-03-06 20:45:26 +0100

    feat: removing unnecessary code and enabling cookieyes in production env

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <!-- Start cookieyes banner -->
-        <script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/8d2c3099b065c4b1f020732641d233c0/script.js"></script>
-        <!-- End cookieyes banner -->
+        @if (app()->environment('production'))
+            <!-- Start cookieyes banner -->
+            <script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/8d2c3099b065c4b1f020732641d233c0/script.js"></script>
+            <!-- End cookieyes banner -->
+        @endif
         
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'MoKumbu') }}</title>
 
         <meta charset="utf-8">
 
@@ -18,13 +20,11 @@
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-
         @vite(['resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+
+    <body>
         @inertia
     </body>
 </html>
