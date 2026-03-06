5d80e7501bfb5c7665449db2600a9dcf324cff88
dorivaldovalentim <dorivaldovalentim@gmail.com>
2026-03-06 20:15:37 +0100

    feat: I guess I finished sitemap.xml and robots.txt setup, for now

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\FirebaseAuthController;
+use App\Http\Controllers\RobotsAndSitemapController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -15,15 +16,5 @@ Route::post('/auth/firebase', FirebaseAuthController::class)->name('auth.firebas
 Route::inertia('/privacy-policy', 'PrivacyPolicy')->name('privacy-policy');
 Route::inertia('/terms-and-conditions', 'TermsAndConditions')->name('terms-and-conditions');
 
-Route::get('/robots.txt', function () {
-    $content = "User-agent: *\n";
-
-    if (app()->environment('production')) {
-        $content .= "Allow: /\nSitemap: " . url('/sitemap.xml');
-    } else {
-        $content .= "Disallow: /";
-    }
-
-    return response($content, 200)
-        ->header('Content-Type', 'text/plain');
-});
+Route::get('/robots.txt', [RobotsAndSitemapController::class, 'robots'])->name('robots');
+Route::get('/sitemap.xml', [RobotsAndSitemapController::class, 'sitemap'])->name('sitemap');
